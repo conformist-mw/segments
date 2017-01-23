4f1f120a1611600ae7cede8d7ba9ae1e6f10a2bd
conformist-mw <conformist.mw@gmail.com>
2017-01-23 10:58:08 +0200

    static files for project

---
+++ static/colors.js
@@ -0,0 +1,5 @@
+var colors = {
+  varnish: ['тут', 'будут', 'цвета', 'some', 'colors'],
+  sateen: ['but', 'nobody', 'added', 'them', 'yet'],
+  dull: ['it', 'is', 'time', 'to', 'wait', 'there']
+};
