d2d9969e3c8df3126dba0e448e4756157f35ef0c
conformist-mw <conformist.mw@gmail.com>
2017-01-27 14:25:23 +0200

    fix default datetime field (thanks kazauwa from devman.slack)

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
@@ -9,8 +10,8 @@ class Segment(db.Model):
     width = db.Column(db.Integer)
     height = db.Column(db.Integer)
     square = db.Column(db.Float)
-    created = db.Column(db.DateTime)
-    deleted = db.Column(db.DateTime)
+    created = db.Column(db.DateTime, default=datetime.now)
+    deleted = db.Column(db.DateTime, default=datetime.now)
     active = db.Column(db.Boolean, default=True)
     order_number = db.Column(db.String(15))
     rack = db.Column(db.String(20))
