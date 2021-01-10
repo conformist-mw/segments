fc4dc0e81cdfeb1ddb6efb7fbb2bf6811929aa44
Oleg Smedyuk <conformist.mw@gmail.com>
2021-01-10 10:55:53 +0200

    added base django application

---
+++ code/config/urls.py
@@ -0,0 +1,11 @@
+import debug_toolbar
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
