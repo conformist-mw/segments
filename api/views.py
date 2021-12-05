dceb871956d3703e473b016cc6337fe05d5f3cbd
Oleg Smedyuk <conformist.mw@gmail.com>
2021-12-05 17:06:35 +0200

    added views to filter related sections within company

--- api/views.py
+++ api/views.py
@@ -1,8 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
 
-from segments.models import Segment
+from segments.models import Segment, Company, Section
 
-from .serializers import SegmentSerializer
+from .serializers import SegmentSerializer, CompanySerializer, SectionSerializer
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all()
+    lookup_field = 'slug'
+
+
+class SectionViewSet(ModelViewSet):
+    serializer_class = SectionSerializer
+    lookup_field = 'slug'
+    queryset = Section.objects.select_related('company').all()
+
+    def get_queryset(self):
+        return self.queryset.filter(company__slug=self.kwargs['company_slug'])
 
 
 class SegmentsViewSet(ModelViewSet):
