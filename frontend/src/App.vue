fe6999bef956f52f8290dc8dee3abd51fbc93295
Oleg Smedyuk <conformist.mw@gmail.com>
2021-12-06 22:53:36 +0200

    added router-view and navbar to the app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,62 +1,19 @@
 <template>
+  <NavBar />
   <div class="container">
-    <div class="grid">
-      <div class="col-4">
-        <h2>Create segment</h2>
-      </div>
-      <div class="col-8">
-        <div v-if="error" class="error">
-          <i class="pi pi-exclamation-triangle"></i> Ошибка!
-        </div>
-        <ProgressSpinner v-else-if="isLoading" class="block right-auto top-50" />
-        <SegmentList v-else :segments="segments" />
-      </div>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-// import Button from 'primevue/button';
-import ProgressSpinner from 'primevue/progressspinner';
-import SegmentList from '@/components/SegmentList.vue';
+import NavBar from '@/components/NavBar.vue';
 
 export default {
   components: {
-    ProgressSpinner,
-    SegmentList,
-    // Button,
-  },
-  data() {
-    return {
-      segments: [],
-      isLoading: false,
-      error: false,
-    };
-  },
-  methods: {
-    fetchSegments() {
-      this.isLoading = true;
-      axios.get('http://127.0.0.1:8000/api/segments/')
-        .then((response) => {
-          this.error = false;
-          this.isLoading = false;
-          this.segments = response.data.results;
-        })
-        .catch(() => {
-          this.error = true;
-        });
-    },
-  },
-  mounted() {
-    this.fetchSegments();
+    NavBar,
   },
 };
 </script>
 
 <style>
-.container {
-  width: 1170px;
-  margin: 0 auto;
-}
 </style>
