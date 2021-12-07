063776f6873d9ec52e28afdc50c8896427199a23
Oleg Smedyuk <conformist.mw@gmail.com>
2021-12-07 23:54:23 +0200

    refactor segments list vue

--- frontend/src/components/SegmentList.vue
+++ frontend/src/components/SegmentList.vue
@@ -9,6 +9,16 @@
   <h2 v-else style="color: red">
     Таких отрезов нет.
   </h2>
+  <button
+  class="btn btn-primary"
+  data-bs-target="#collapseTarget"
+  data-bs-toggle="collapse">
+  Bootstrap collapse
+</button>
+<div class="collapse py-2" id="collapseTarget">
+  This is the toggle-able content!
+</div>
+
 </template>
 
 <script>
