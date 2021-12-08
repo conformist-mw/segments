4ff6ff537d4e63bc4c4ac4a1ff4f91bb4eca9b72
Oleg Smedyuk <conformist.mw@gmail.com>
2021-12-09 00:30:36 +0200

    use separate page for segments

---
+++ frontend/src/pages/SegmentsPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 class="my-5">Отрезки!</h1>
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-8">
+      <SegmentList :segments="segments" />
+    </div>
+  </div>
+</template>
+
+<script>
+import useSegments from '../hooks/useSegments';
+import SegmentList from '../components/SegmentList.vue';
+
+export default {
+  components: {
+    SegmentList,
+  },
+  setup() {
+    const { segments, error, isSegmentsLoading } = useSegments();
+
+    return {
+      segments, error, isSegmentsLoading,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
