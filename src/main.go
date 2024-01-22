f91993d1a79764d309d45e7ba01a822da24648a8
Oleg Smedyuk <oleg.smedyuk@gmail.com>
2024-01-22 20:23:55 +0200

    finish segment template

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"text/template"
 	"time"
@@ -21,8 +22,28 @@ func init() {
 	}
 }
 
+type TemplateContext struct {
+	Segment models.Segment // replace SegmentType with the type of your segments
+	Racks   []models.Rack  // replace RackType with the type of your racks
+}
+
 func FormatInLocation(t time.Time) string {
 	return t.In(location).Format("02.01.06 | 15:04")
+}
+
+func dict(values ...interface{}) (map[string]interface{}, error) {
+	if len(values)%2 != 0 {
+		return nil, errors.New("invalid dict call")
+	}
+	dict := make(map[string]interface{}, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		key, ok := values[i].(string)
+		if !ok {
+			return nil, errors.New("dict keys must be strings")
+		}
+		dict[key] = values[i+1]
+	}
+	return dict, nil
 }
 
 func main() {
@@ -30,6 +51,7 @@ func main() {
 	router := gin.Default()
 	router.SetFuncMap(template.FuncMap{
 		"formatInLocation": FormatInLocation,
+		"dict":             dict,
 	})
 	router.Static("/static", "./static")
 	router.LoadHTMLGlob("templates/*")
