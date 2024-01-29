21de805fc43e92e03af585adcf4ecc5b7c2f6e29
Oleg Smedyuk <oleg.smedyuk@gmail.com>
2024-01-29 11:21:03 +0200

    remove django implementation

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.10-bullseye
-
-COPY requirements.txt /tmp/
-RUN pip install --no-cache-dir -r /tmp/requirements.txt
-
-COPY code /code/
-WORKDIR /code
--- code/segments/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .colors import Color, ColorType  # noqa: F401
-from .locations import Company, Rack, Section  # noqa: F401
-from .orders import OrderNumber  # noqa: F401
-from .segments import Segment, SegmentOld  # noqa: F401
